7368ffb93ffe55e9c5c809a29798ad81729e10dd
DVK <42199853+DVKunion@users.noreply.github.com>
2022-11-04 10:38:36 +0800

    feat(iac): complete validator.go (#43)

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/chaitin/libveinmind
 go 1.14
 
 require (
-	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/moby/buildkit v0.10.5
 	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417
 	github.com/pkg/errors v0.9.1
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.25.2
 	k8s.io/apimachinery v0.25.2
 	k8s.io/cli-runtime v0.25.2
--- go/iac/discover.go
+++ go/iac/discover.go
@@ -9,10 +9,17 @@ import (
 // discoverOption is the internal data to modify the way
 // of discovering iacs.
 type discoverOption struct {
+	iacType IACType
 }
 
 // DiscoverOption specifies how to find and validate iacs.
 type DiscoverOption func(*discoverOption)
+
+func WithIaCType(iacType IACType) DiscoverOption {
+	return func(o *discoverOption) {
+		o.iacType = iacType
+	}
+}
 
 // newDiscoverOption creates the discover option object.
 func newDiscoverOption(opts ...DiscoverOption) *discoverOption {
@@ -24,22 +31,28 @@ func newDiscoverOption(opts ...DiscoverOption) *discoverOption {
 }
 
 func DiscoverIACs(root string, opts ...DiscoverOption) ([]IAC, error) {
-	_ = newDiscoverOption(opts...)
+	discoverOpt := newDiscoverOption(opts...)
 	var results []IAC
 
 	err := filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		// An IaC File Must Be An Regular File
+		if !info.Mode().IsRegular() {
+			return nil
+		}
 		validators.Range(func(key, value interface{}) bool {
-			t := key.(IACType)
-
 			if v, ok := value.(Validator); ok {
-				if v.Validate(path, info) {
-					results = append(results, IAC{
-						Path: path,
-						Type: t,
-					})
+				if discoverOpt.iacType == "" || discoverOpt.iacType == v.ID() {
+					if v.Validate(path, info) {
+						// add opt func
+						results = append(results, IAC{
+							Path: path,
+							Type: v.ID(),
+						})
+						// if pass a validator, no need to check more
+						return false
+					}
 				}
 			}
-
 			return true
 		})
 		return nil
