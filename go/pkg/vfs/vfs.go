c2cdcf0e2ccb3dbeb054665735c4e786fb25a511
DVK <42199853+DVKunion@users.noreply.github.com>
2023-02-14 16:57:50 +0800

    fix(vfs): relative path (#60)

--- go/pkg/vfs/vfs.go
+++ go/pkg/vfs/vfs.go
@@ -9,27 +9,42 @@ func rootfs() string {
 	if v := os.Getenv("LIBVEINMIND_HOST_ROOTFS"); v != "" {
 		return v
 	} else {
-		return "/"
+		return ""
 	}
 }
 
 func Open(name string) (*os.File, error) {
+	if path, err := filepath.Abs(name); err == nil {
+		name = path
+	}
 	return os.Open(filepath.Join(rootfs(), name))
 }
 
 func Stat(name string) (os.FileInfo, error) {
+	if path, err := filepath.Abs(name); err == nil {
+		name = path
+	}
 	return os.Stat(filepath.Join(rootfs(), name))
 }
 
 func Lstat(name string) (os.FileInfo, error) {
+	if path, err := filepath.Abs(name); err == nil {
+		name = path
+	}
 	return os.Lstat(filepath.Join(rootfs(), name))
 }
 
 func Readlink(name string) (string, error) {
+	if path, err := filepath.Abs(name); err == nil {
+		name = path
+	}
 	return os.Readlink(filepath.Join(rootfs(), name))
 }
 
 func Readdir(name string) ([]os.DirEntry, error) {
+	if path, err := filepath.Abs(name); err == nil {
+		name = path
+	}
 	return os.ReadDir(filepath.Join(rootfs(), name))
 }
 
